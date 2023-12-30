551b073d59695a8e01103777e876cbf7f7c69635
Deabou <bourgeo282@gmail.com>
2023-12-30 17:21:03 -0600

    initial upload for discord bot

---
+++ mr_planner/target/debug/deps/mr_planner-cfb427b50f54b010.d
@@ -0,0 +1,5 @@
+C:\Users\Chris\Python\discord_mr_planner\mr_planner\target\debug\deps\libmr_planner-cfb427b50f54b010.rmeta: src\main.rs
+
+C:\Users\Chris\Python\discord_mr_planner\mr_planner\target\debug\deps\mr_planner-cfb427b50f54b010.d: src\main.rs
+
+src\main.rs:
